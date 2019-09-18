f7f6bc8375ca1aeea55c38da79a6f3c4b0f5dcf3
SentientSam <46612325+SentientSam@users.noreply.github.com>
2019-09-18 14:20:49 -0500

    Create cstringsort.h

---
+++ cstringsort.h
@@ -0,0 +1,16 @@
+/*
+  cstringsort.h
+  1/25/2019
+  Samuel Lamb
+  Project 1
+*/
+
+
+#ifndef _CSTRINGSORT_H
+#define _CSTRINGSORT_H
+
+void LexDiff (char* *beg, char* *end);
+void DictionaryDiff (char* *beg, char* *end);
+
+
+#endif
